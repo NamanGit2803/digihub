1f36f9fb56a322eb82d769cf3aca34c152609512
Naman <njain4282@gmail.com>
2026-05-16 13:53:22 +0530

    update route3

--- app/api/admin/users/route.js
+++ app/api/admin/users/route.js
@@ -3,7 +3,6 @@ export const dynamic = "force-dynamic"
 import { NextResponse } from "next/server"
 import { prisma } from "@/lib/prisma"
 
-const prisma = new PrismaClient()
 
 export async function GET() {
     try {
